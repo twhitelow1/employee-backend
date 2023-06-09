48dfaeb657d4efdaa9fd0b21be0d97a22b9595ee
Todd Whitelow <todd@whitelow.tech>
2023-06-08 22:44:11 -0600

    add positions actions in the controller

--- app/controllers/api/positions_controller.rb
+++ app/controllers/api/positions_controller.rb
@@ -3,4 +3,32 @@ class Api::PositionsController < ApplicationController
     @positions = Position.all
     render "index.json.jb"
   end
+
+  def create
+    @position = Position.new(
+      name: params[:name],
+      description: params[:description],
+    )
+    @position.save
+    render "show.json.jb"
+  end
+
+  def show
+    @position = Position.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @position = Position.find_by(id: params[:id])
+    @position.name = params[:name] || @position.name
+    @position.description = params[:description] || @position.description
+    @position.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    position = Position.find_by(id: params[:id])
+    position.destroy
+    render json: {message: "Position successfully destroyed."}
+  end
 end
